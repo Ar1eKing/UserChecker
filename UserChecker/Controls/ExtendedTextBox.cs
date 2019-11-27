52c289778e7fef4e5c36f96adb03096e0dfa90bc
Vlad <valex.win@gmail.com>
2019-11-27 20:05:37 +0200

    [-] old validation [+] new validation

---
+++ UserChecker/Controls/ExtendedTextBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+
+namespace UserChecker.Controls
+{
+    public class ExtendedTextBox : TextBox
+    {
+        public static readonly DependencyProperty IsNumericOnlyProperty
+            = DependencyProperty.Register(nameof(IsNumericOnly), typeof(bool), typeof(ExtendedTextBox));
+
+        public bool IsNumericOnly
+        {
+            get { return (bool)GetValue(IsNumericOnlyProperty); }
+            set { SetValue(IsNumericOnlyProperty, value); }
+        }
+
+        public ExtendedTextBox() => Init();
+
+        private void Init()
+        {
+            IsNumericOnly = false;
+            base.PreviewTextInput += ExtendedTextBox_PreviewTextInput;
+        }
+
+        private void ExtendedTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (IsNumericOnly)
+            {
+                Regex regex = new Regex("[^0-9]+");
+                e.Handled = regex.IsMatch(e.Text);
+            }
+        }
+    }
+}
